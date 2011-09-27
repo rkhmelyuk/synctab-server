243c2a39c28ad861b87264c21f1f9c6193cd51f2
Ruslan Khmelyuk <ruslan.khmelyuk@gmail.com>
2011-09-27 23:05:32 +0300

    Create default user.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,19 @@
+import com.khmlabs.synctab.UserService
+import com.khmlabs.synctab.User
+
 class BootStrap {
 
+    UserService userService
+
     def init = { servletContext ->
+        User user = userService.getUserByEmail("ruslan@khmelyuk.com")
+        if (user == null) {
+            user = new User()
+            user.email = "ruslan@khmelyuk.com"
+            userService.registerUser(user, "qwerty")
+        }
     }
+
     def destroy = {
     }
 }
