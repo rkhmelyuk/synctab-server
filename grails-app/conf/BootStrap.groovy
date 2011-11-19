f5a9bf0a8438716242b101d3fc250c07a013b612
Ruslan Khmelyuk <ruslan.khmelyuk@gmail.com>
2011-11-20 00:07:12 +0200

    Version upgrading facility.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,19 +1,33 @@
-import com.khmlabs.synctab.UserService
 import com.khmlabs.synctab.User
+import com.khmlabs.synctab.UserService
+import com.khmlabs.synctab.upgrade.VersionUpgrade
+import grails.util.GrailsUtil
+import org.codehaus.groovy.grails.commons.GrailsApplication
 
 class BootStrap {
 
     UserService userService
+    GrailsApplication grailsApplication
 
     def init = { servletContext ->
         User user = userService.getUserByEmail('ruslan@khmelyuk.com')
         if (user == null) {
             user = new User()
             user.email = 'ruslan@khmelyuk.com'
-            userService.registerUser(user, 'qwerty')
+            userService.registerUser(user, 'gop2ca')
         }
-        else {
-            userService.changePassword(user, 'gop2ca')
+
+        switch (GrailsUtil.environment) {
+            case 'production':
+                // upgrade to the newer version if any
+                String version = grailsApplication.metadata['app.version']
+                new VersionUpgrade(grailsApplication.mainContext).upgradeToVersion(version)
+                break
+            case 'staging': case 'development':
+                new VersionUpgrade(grailsApplication.mainContext).upgrade()
+                break
+            case 'test':
+                break
         }
     }
 
